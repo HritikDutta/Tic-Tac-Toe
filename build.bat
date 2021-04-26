803df5c8b36144d7f8acf58473488c015a13aae9
ThreewayDude <dutta.hritik@gmail.com>
2021-04-26 21:30:18 +0530

    Now the game doesn't launch a cmd window whenever it starts

--- build.bat
+++ build.bat
@@ -4,19 +4,19 @@ set includes=/I dependencies\Glad\include /I dependencies\GLFW\include /I src /I
 set libs=dependencies\GLFW\lib\glfw3.lib dependencies\Glad\lib\glad.lib Shell32.lib User32.lib Gdi32.lib OpenGL32.lib msvcrt.lib
 
 rem Libraries
-cl /EHsc /c dependencies\stb\src\*.cpp  /I dependencies\stb\include
+cl /Ox /EHsc /c dependencies\stb\src\*.cpp  /I dependencies\stb\include
 
-cl /EHsc /c src/universal/vecs/*.cpp
-cl /EHsc /c src/universal/mats/*.cpp
-cl /EHsc /c src/universal/*.cpp
+cl /MT /Ox /EHsc /c src/universal/vecs/*.cpp
+cl /MT /Ox /EHsc /c src/universal/mats/*.cpp
+cl /MT /Ox /EHsc /c src/universal/*.cpp
 
-cl /EHsc /c src/engine/*.cpp %includes%
-cl /EHsc /c src/platform/*.cpp %includes%
+cl /MT /Ox /EHsc /c src/engine/*.cpp %includes%
+cl /MT /Ox /EHsc /c src/platform/*.cpp %includes%
 
-cl /EHsc /c src/game/*.cpp %includes%
+cl /MT /Ox /EHsc /c src/game/*.cpp %includes%
 
-cl /EHsc /c src/main.cpp %includes%
+cl /MT /Ox /EHsc /c src/main.cpp %includes%
 
-link *.obj %libs% /OUT:ttt.exe /NODEFAULTLIB:LIBCMT
+link *.obj %libs% /OUT:ttt.exe /NODEFAULTLIB:LIBCMT /SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup
 
 del *.obj
