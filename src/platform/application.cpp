bb0bb1774502c648620dcb07e596ec05ac3b47a1
ThreewayDude <dutta.hritik@gmail.com>
2021-04-26 13:54:18 +0530

    Added back-face culling cause why not :P

--- src/platform/application.cpp
+++ src/platform/application.cpp
@@ -80,9 +80,9 @@ Application::Application(const char title[], int width, int height, bool fullscr
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    // glEnable(GL_CULL_FACE);
-    // glCullFace(GL_BACK);
-    // glFrontFace(GL_CCW);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CCW);
 
     glEnable(GL_DEPTH_TEST);
 
